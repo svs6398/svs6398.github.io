80407055e36438cff924b14771c9a205b40b77fc
svs6398 <58524371+svs6398@users.noreply.github.com>
2019-12-06 17:42:09 -0500

    Add files via upload

---
+++ RecordPlayer.js
@@ -0,0 +1,47 @@
+function hideRecordPlayer() {
+  document.getElementById("RecordPlayerStill").style.visibility = "hidden";
+}
+
+function showRecordPlayer() {
+  document.getElementById("RecordPlayerStill").style.visibility = "visible";
+}
+
+var music1 = document.getElementById("BeachBoys");
+function playBeachBoys() {
+  music1.play();
+}
+function pauseBeachBoys() {
+  music1.pause();
+}
+
+var music2 = document.getElementById("TheDoors");
+function playTheDoors() {
+  music2.play();
+}
+function pauseTheDoors() {
+  music2.pause();
+}
+
+var music3 = document.getElementById("KingCrimson");
+function playKingCrimson() {
+  music3.play();
+}
+function pauseKingCrimson() {
+  music3.pause();
+}
+
+var music4 = document.getElementById("PinkFloyd");
+function playPinkFloyd() {
+  music4.play();
+}
+function pausePinkFloyd() {
+  music4.pause();
+}
+
+var music5 = document.getElementById("TheBeatles");
+function playTheBeatles() {
+  music5.play();
+}
+function pauseTheBeatles() {
+  music5.pause();
+}
